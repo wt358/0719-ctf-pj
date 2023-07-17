7eac991ee335e2e400aad33f20c1f991d650e2c8
djc06048 <gsafe1213@gmail.com>
2023-07-17 14:14:28 +0900

    [feat] 모델 성능 테이블 추가

---
+++ frontend/src/components/anomaly/ModelPerformance.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1"></v-data-table>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+export default {
+  computed: {
+    ...mapState(["mpHeaders"]),
+    ...mapState(["mpDateSets"]),
+  },
+};
+</script>
+
+<style></style>
