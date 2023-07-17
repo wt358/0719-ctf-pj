c321da1d3a6fcb54cedb30f518ca8142ed5b6c30
djc06048 <gsafe1213@gmail.com>
2023-07-17 14:16:39 +0900

    [fix] 이상감지 페이지 수정

--- frontend/src/views/AnomalyDetection.vue
+++ frontend/src/views/AnomalyDetection.vue
@@ -2,18 +2,30 @@
   <v-container>
     <v-row>
       <v-col cols="12" sm="12" md="8" lg="8" xl="12">
-        <Outlier-distribution></Outlier-distribution>
+        <v-card class="pa-3" outlined style="height: 100%">
+          <h3>단계별 이상치 분포</h3>
+          <anomaly-line-chart></anomaly-line-chart>
+        </v-card>
       </v-col>
       <v-col cols="12" sm="12" md="4" lg="4" xl="12">
-        <Outlier-weight></Outlier-weight>
+        <v-card class="pa-3" outlined style="height: 100%">
+          <h3>단계별 이상치 비중</h3>
+          <anomaly-circle-chart></anomaly-circle-chart>
+        </v-card>
       </v-col>
       <v-col cols="8" sm="12" md="8" lg="8" xl="12">
         <v-row>
           <v-col cols="12">
-            <Time-Model></Time-Model>
+            <v-card class="pa-3" outlined style="height: 100%">
+              <h3>모델별 학습 소요시간</h3>
+              <model-timebar-chart></model-timebar-chart>
+            </v-card>
           </v-col>
           <v-col cols="12">
-            <performance-model></performance-model>
+            <v-card class="pa-3" outlined style="height: 100%">
+              <h3>모델별 성능</h3>
+              <model-performance></model-performance>
+            </v-card>
           </v-col>
         </v-row>
       </v-col>
@@ -25,18 +37,19 @@
 </template>
 
 <script>
-import OutlierDistribution from "../components/anomaly/OutlierDistribution.vue";
-import OutlierWeight from "@/components/anomaly/OutlierWeight.vue";
-import PerformanceModel from "../components/anomaly/PerformanceModel.vue";
-import TimeModel from "../components/anomaly/TimeModel.vue";
 import ModelSummary from "@/components/anomaly/ModelSummary.vue";
+
+import AnomalyCircleChart from "@/components/chart/circle/AnomalyCircleChart.vue";
+import AnomalyLineChart from "@/components/chart/line/AnomalyLineChart.vue";
+import ModelTimebarChart from "@/components/chart/bar/ModelTimebarChart.vue";
+import ModelPerformance from "@/components/anomaly/ModelPerformance.vue";
 export default {
   components: {
-    OutlierDistribution,
-    OutlierWeight,
-    PerformanceModel,
-    TimeModel,
     ModelSummary,
+    AnomalyCircleChart,
+    AnomalyLineChart,
+    ModelTimebarChart,
+    ModelPerformance,
   },
 };
 </script>
