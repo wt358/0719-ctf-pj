297b26bc8edb414a59a6ae4879b330526c832b7d
djc06048 <gsafe1213@gmail.com>
2023-07-17 14:14:51 +0900

    [feat] 차트 추가

---
+++ frontend/src/components/chart/circle/AnomalyCircleChart.vue
@@ -0,0 +1,54 @@
+<template>
+  <div><canvas ref="pieChartCanvas" width="200" height="200"></canvas></div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+import Chart from "chart.js";
+
+export default {
+  computed: {
+    ...mapState(["ratios"]),
+  },
+  mounted() {
+    this.drawChart();
+  },
+  created() {
+    this.$store.dispatch("fetchAnomalyList");
+  },
+  methods: {
+    ...mapActions(["fetchAnomalyList"]),
+    drawChart() {
+      const ctx = this.$refs.pieChartCanvas.getContext("2d");
+      const labels = Object.keys(this.ratios);
+      const data = Object.values(this.ratios);
+
+      new Chart(ctx, {
+        type: "pie",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              data: data,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.8)",
+                "rgba(54, 162, 235, 0.8)",
+                "rgba(255, 206, 86, 0.8)",
+                // Add more colors if needed
+              ],
+              borderColor: "rgba(0, 0, 0, 0.8)",
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          // Additional options for customization
+        },
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
