836fe775b3dadc1de3e9d01604ae37e3af5b0dc7
djc06048 <gsafe1213@gmail.com>
2023-07-18 17:11:25 +0900

    [feat] 평균온도 임시 데이터로 1차표현

--- frontend/src/components/chart/AverageTempGraph.vue
+++ frontend/src/components/chart/AverageTempGraph.vue
@@ -1,9 +1,36 @@
 <template>
-  <v-card class="rounded pa-3"> 평균온도 원형그래프 </v-card>
+  <div><canvas ref="doughnutCanvas"></canvas></div>
 </template>
 
 <script>
-export default {};
+import Chart from "chart.js";
+
+export default {
+  mounted() {
+    this.drawChart();
+  },
+  methods: {
+    drawChart() {
+      const ctx = this.$refs.doughnutCanvas.getContext("2d");
+      new Chart(ctx, {
+        type: "doughnut",
+        data: {
+          labels: ["Label 1", "Rest"],
+          datasets: [
+            {
+              data: [0.7, 0.3],
+              backgroundColor: ["yellow", "lightgray"],
+            },
+          ],
+        },
+        options: {
+          // 추가적인 옵션 설정 가능
+          responsive: true,
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style></style>
